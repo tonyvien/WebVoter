be62e71cf6fea109382b8d394d53656b043752df
tonyvien <tonyvien@mail.usf.edu>
2017-11-28 04:40:07 -0500

    Add files via upload

--- utilities.rb
+++ utilities.rb
@@ -1,4 +1,27 @@
 require 'csv'
+require 'zip'
+require 'bcrypt'
+require 'sqlite3'
+
+# Opens a connection to the database and returns the reference to it for
+# later use. This function first checks if a database (named 'webvoter') - located 
+# in the current folder - already exists to prevent creating a new DB each time this
+# function is called.
+def openDB()
+	unless File.exist?('./webvoter.database')
+		db = SQLite3::Database.new( "./webvoter.database" )
+		# Set up the base table with uid, username, password, and role
+		db.execute( "CREATE TABLE Class (id INTEGER PRIMARY KEY, user TEXT, password TEXT, role TEXT);" )
+		# Add an extra columns for the user's vote status, 1st, 2nd, and 3rd vote
+		db.execute ("ALTER TABLE Class ADD COLUMN has_voted INTEGER DEFAULT 0; ")
+		db.execute ("ALTER TABLE Class ADD COLUMN first_choice TEXT DEFAULT 'NONE'; ")
+		db.execute ("ALTER TABLE Class ADD COLUMN second_choice TEXT DEFAULT 'NONE'; ")
+		db.execute ("ALTER TABLE Class ADD COLUMN third_choice TEXT DEFAULT 'NONE'; ")
+	else
+		db = SQLite3::Database.open( "./webvoter.database" )
+	end
+	return db
+end
 
 
 # Checks if CSV is valid before adding users to db. Each
@@ -14,20 +37,25 @@ def validateCSV(path)
 end
 
 
-
-# Add each user to the DB
-def csvToDB(path)
-	file_contents = ""
+# This function adds each student/instructor to the database from
+# the user uploaded CSV file. Passwords are hashed when storing
+# each user's information and returns true if insertion is 
+# successful, false otherwise. Parameter is a string path to
+# to uploaded CSV file. 
+def importCSV(path)
 	if validateCSV(path)
-
+		# Open DB connection
+		db = openDB()
 		# Add users to DB inside this loop
 		CSV.foreach(path) do |col|
-			unless (col[0] == "users") # if csv contains headers
-				file_contents += col[0] + col[1] + col[2] + "\n"
-				# TODO: Add users to DB here (col[0] = user, HASH(col[1]) = password, col[2] = role)
-				#
-				#
-				#
+			unless (col[0] == "users") # prevent storing any possible headers
+				username = col[0].strip
+				password = col[1].strip
+				role = col[2].strip
+				# Hash the current user's password
+				hashed_pw = BCrypt::Password.create(password)
+				# Store current user into DB
+				db.execute( "INSERT INTO Class (user, password, role) VALUES ('#{username}', '#{hashed_pw}', '#{role}')" )
 			end
 		end
 		return true
@@ -35,3 +63,91 @@ def csvToDB(path)
 		return false
 	end
 end
+
+
+# This function Imports ZIP files and stores them in the website folder 
+# (Found in public folder). Any changes to the desired exporting path can 
+# be made by changing 'public/ws'
+def importZIP(path)
+	dest_folder = File.join(Dir.pwd, 'public/ws')
+	Zip::File.open(path) do |zipfile|
+	  zipfile.each do |entry|
+	  	cur_file = File.join(dest_folder, entry.name)
+	    unless File.exist?(cur_file)	
+	      FileUtils::mkdir_p(File.dirname('public/ws/'+entry.name)) # Create website dir
+	      zipfile.extract(entry, cur_file)	# Extract files into dir
+	    end
+	  end
+	end
+	return true
+end
+
+
+# This function first checks if username exists, if it does
+# check to see if its password is correct. If username does not 
+# exist or password is incorrect, return false. Parameter for the
+# role is passed to indicate the where the user is trying to log
+# in from. (Either the Student or Instructor Login portal).
+def verifyUser(username, password, role)
+	db = openDB() # Open DB connection
+
+	# default query string. This will be passed into the db.execute query function below
+	qry = "SELECT * FROM Class WHERE user='#{username}'"
+
+	# add a query filter for students or instructors/tas
+	if(role=="student")
+		qry += " AND role='student'"
+	else
+		qry += " AND (role='instructor' or role='ta')"
+	end
+
+	# user holds an array of user information returned from DB
+	# where user[0] holds the id, user[1] the user, user[2] the password, etc.
+	user = (db.execute( qry ))[0]
+
+	# If the user was found, move on to validate the password
+	if user
+		# Compare the user's stored encrypted password with the submitted one
+		stored_pw = BCrypt::Password.new(user[2])
+		if(stored_pw == password)
+			return user[0] # return user's id
+		else
+			return false
+		end
+	else # user was not found, return false
+		return false
+	end
+end
+
+
+
+def getSites()
+	ws = []
+	#gets all the html files located in the 'public/ws' folder
+	Dir['./public/ws/*/*.html'].each {|x|
+		ws.push(x.gsub('./public/', ''))
+	}
+	return  ws
+end
+
+
+
+def processVote(uid, first, second, third)
+	db = openDB()
+	# Checks if the user has voted. voteStatus should be 0 if user hasn't voted yet
+	voteStatus = db.execute("SELECT has_voted FROM Class WHERE id=#{uid}")[0][0]
+	# If the user hasn't voted yet, update their choices and flag that they voted.
+	if(voteStatus == 0)
+		db.execute("UPDATE Class 
+					SET has_voted=1, 
+						first_choice='#{first}',
+						second_choice='#{second}',
+						third_choice='#{third}'
+					WHERE id=#{uid}")
+		return true
+	else
+		# If user has already voted, return false and show error
+		return false
+	end
+end
+
